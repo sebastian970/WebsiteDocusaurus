4bee868a23e6f972600274b2145f65ced850c406
Oleg <creduntvitam@gmail.com>
2019-11-05 08:56:44 +0100

    Second commit

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,13 +30,13 @@ const siteConfig = {
   title: 'Documentation', // Title for your website.
   tagline: 'Future Business',
   url: 'https://OlegGitH.github.io', // Your website URL
-  baseUrl: '/MasterFuture/', // Base URL for your project */
+  baseUrl: '/FutureMasteres/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'MasterFuture',
+  projectName: 'FutureMasters',
   organizationName: 'OlegGitH',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
